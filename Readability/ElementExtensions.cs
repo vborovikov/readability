bea4dc4e88fe0baf185fbc922458c6b3ad253b4a
Vladislav Borovikov <vborovikov@live.ru>
2025-01-03 05:46:02 +0500

    created IsProbablyHidden method

---
+++ Readability/ElementExtensions.cs
@@ -0,0 +1,29 @@
+﻿namespace Readability;
+
+using Brackets;
+
+static class ElementExtensions
+{
+    public static bool IsProbablyHidden(this Tag tag)
+    {
+        if (!tag.HasAttributes)
+            return false;
+
+        if (tag.Attributes["style"] is { Length: > 0 } style)
+        {
+            foreach (var cssDeclaration in style.EnumerateCssDeclarations())
+            {
+                if (cssDeclaration.Property is "display" && cssDeclaration.Value is "none")
+                    return true;
+                if (cssDeclaration.Property is "visibility" && cssDeclaration.Value is "hidden")
+                    return true;
+            }
+        }
+
+        return 
+            tag.Attributes.Has("hidden") || 
+            tag.Attributes.Has("aria-hidden", "true") || 
+            tag.Attributes.Has("class", "hidden") ||
+            tag.Attributes.Has("type", "hidden");
+    }
+}
